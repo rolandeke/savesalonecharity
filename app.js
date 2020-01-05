7f3a36aeca80395a01133c139e471e2e7c02868c
rolandeke <rolandeke49@gmail.com>
2020-01-05 14:30:52 +0000

    Moved project to a nodejs project and added some contents to the our-team section

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+
+// app.use(express.static('public'))
+
+// app.get('/', (req, res) =>
+// {
+//     res.sendFile('index.html')
+// })
+
+app.post('/contact', (req, res) =>
+{
+    console.log(req.body)
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+{
+    console.log(`Server started on port ${port}`)
+})
