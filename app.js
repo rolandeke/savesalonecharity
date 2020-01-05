23efcb622d017c075abcd4a2d78aca51f73ce2bd
rolandeke <rolandeke49@gmail.com>
2020-01-05 14:59:43 +0000

    added static files to e used by app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express();
 
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
-// app.get('/', (req, res) =>
-// {
-//     res.sendFile('index.html')
-// })
+app.get('/', (req, res) =>
+{
+    res.sendFile('index.html')
+})
 
 app.post('/contact', (req, res) =>
 {
