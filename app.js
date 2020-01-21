25908d974fe3201a8ba55e5b7e0e80726853a18a
rolandeke <rolandeke49@gmail.com>
2020-01-21 13:49:14 +0000

    Added body parser and nodemailer and also created a post router for sending contact form

--- app.js
+++ app.js
@@ -1,21 +1,56 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 const app = express();
 
-app.use(express.static('public'))
+dotenv.config();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) =>
-{
-    res.sendFile('index.html')
-})
+//GET: Index.html
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
 
-app.post('/contact', (req, res) =>
-{
-    console.log(req.body)
-})
-
+//POST: COntact form from index.html
+app.post("/contact", async (req, res) => {
+  try {
+    const { username, email, message } = req.body;
+    const info = await sendMail(username, email, message);
+    console.log(info);
+  } catch (error) {
+    throw error;
+  }
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
-{
-    console.log(`Server started on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+sendMail = async (email, username, message) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.EMAIL, // user's email
+        pass: process.env.PASSWORD // user's password
+      }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: email, // sender address
+      to: "rolandeke49@gmail.com", // list of receivers
+      subject: `New Message From Contact Form by -  ${username}`, // Subject line
+      text: message
+    });
+
+    return info;
+  } catch (error) {
+    throw error;
+  }
+};
