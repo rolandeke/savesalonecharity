cd71e88ee593fe6fb6ff51901423c8487287f12e
rolandeke <rolandeke49@gmail.com>
2020-01-21 00:51:09 +0000

    Added bodyparser for implementing contact form

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 const express = require('express')
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
 const app = express();
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.set("view engine", "ejs")
 
-app.get('/', (req, res) =>
-{
-    res.sendFile('index.html')
+
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: "Test"
+    })
 })
 
-app.post('/contact', (req, res) =>
-{
-    console.log(req.body)
+
+app.post('/', (req, res) => {
+    const contactObj = {
+        username: req.body.username,
+        email: req.body.email,
+        message: req.body.message
+    }
+    console.log(contactObj)
+
 })
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
-{
+app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
