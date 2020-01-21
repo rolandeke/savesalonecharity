a5ebbe74b83fd5b8b6c383699daa88e458b55637
rolandeke <rolandeke49@gmail.com>
2020-01-21 01:18:29 +0000

    Resolved all errors

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
@@ -29,7 +28,7 @@ app.post('/', (req, res) => {
     console.log(contactObj)
 
 })
-=======
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
@@ -38,57 +37,61 @@ const app = express();
 
 dotenv.config();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 //GET: Index.html
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+    res.sendFile("index.html");
 });
->>>>>>> 25908d974fe3201a8ba55e5b7e0e80726853a18a
+
 
 //POST: COntact form from index.html
 app.post("/contact", async (req, res) => {
-  try {
-    const { username, email, message } = req.body;
-    const info = await sendMail(username, email, message);
-    console.log(info);
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const {
+            username,
+            email,
+            message
+        } = req.body;
+        const info = await sendMail(username, email, message);
+        console.log(info);
+    } catch (error) {
+        throw error;
+    }
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-<<<<<<< HEAD
+
     console.log(`Server started on port ${port}`)
 })
-=======
-  console.log(`Server started on port ${port}`);
-});
+
+
 
 sendMail = async (email, username, message) => {
-  try {
-    let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: process.env.EMAIL, // user's email
-        pass: process.env.PASSWORD // user's password
-      }
-    });
+    try {
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: process.env.EMAIL, // user's email
+                pass: process.env.PASSWORD // user's password
+            }
+        });
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: email, // sender address
-      to: "rolandeke49@gmail.com", // list of receivers
-      subject: `New Message From Contact Form by -  ${username}`, // Subject line
-      text: message
-    });
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: email, // sender address
+            to: "rolandeke49@gmail.com", // list of receivers
+            subject: `New Message From Contact Form by -  ${username}`, // Subject line
+            text: message
+        });
 
-    return info;
-  } catch (error) {
-    throw error;
-  }
+        return info;
+    } catch (error) {
+        throw error;
+    }
 };
->>>>>>> 25908d974fe3201a8ba55e5b7e0e80726853a18a
